233b775903b08a45ddf7ec270a8d78a5c8ed77b5
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-08-17 00:37:57 -0700

    ♦️ first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Voting Contract 
+The following contract is quite complex, but showcases a lot of Solidity’s features. It implements a voting contract. Of course, the main problems of electronic voting is how to assign voting rights to the correct persons and how to prevent manipulation. We will not solve all problems here, but at least we will show how delegated voting can be done so that vote counting is automatic and completely transparent at the same time.
+
+The idea is to create one contract per ballot, providing a short name for each option. Then the creator of the contract who serves as chairperson will give the right to vote to each address individually.
+
+The persons behind the addresses can then choose to either vote themselves or to delegate their vote to a person they trust.
+
+At the end of the voting time, winningProposal() will return the proposal with the largest number of votes.
+
+## Getting Started
+
+### Installation
+
+-copy all files/folders from project2 except artifacts, cache, node_modules
+-create new project3 and paste here
+-make sure you have the .env file (.gitignore it)
+create a scripts folder under the root project4 folder & then create a DeployWithHardhat.ts file under scripts directory
+
+### Hardhat 
+```shell
+nvm use --lts   
+node -v
+v20.14.0
+```
+
+## Hardhat 
+```shell
+npm init
+npm install --save-dev hardhat
+npx hardhat init
+```
+-> Create a typescript project
+
+```shell
+npx hardhat compile
+npx hardhat test
+npx hardhat clean
+
+
+cretae a .mocharc.json with contents:
+{
+  "require": "hardhat/register",
+  "timeout": 40000,
+  "_": ["test*/**/*.ts"]
+}
+
+
+rm ./contracts/*
+rm ./ignition/*
+rm ./test/*
+npx hardhat clean
+npm viem
+npm install --save-dev @nomicfoundation/hardhat-chai-matchers
+```
+
---
+++ scripts/CastVote.ts
@@ -0,0 +1,100 @@
+import { sepolia } from "viem/chains";
+import * as dotenv from "dotenv";
+import { abi, bytecode } from "../artifacts/contracts/Ballot.sol/Ballot.json";
+
+
+dotenv.config();
+
+import { createPublicClient, http, createWalletClient, formatEther, toHex, hexToString } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+
+async function main() {
+  const proposals = process.argv.slice(2);
+  if (!proposals || proposals.length < 1)
+    throw new Error("Proposals not provided");
+  const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+  const blockNumber = await publicClient.getBlockNumber();
+  console.log("Last block number:", blockNumber);
+  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
+  const deployer = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+  console.log("Deployer address:", deployer.account.address);
+  const balance = await publicClient.getBalance({
+    address: deployer.account.address,
+  });
+
+  //
+  console.log(
+    "Deployer balance:",
+    formatEther(balance),
+    deployer.chain.nativeCurrency.symbol
+  );
+  
+  //
+  // console.log("\nDeploying Ballot contract");
+  // const hash = await deployer.deployContract({
+  //   abi,
+  //   bytecode: bytecode as `0x${string}`,
+  //   args: [proposals.map((prop) => toHex(prop, { size: 32 }))],
+  // });
+  // console.log("Transaction hash:", hash);
+  // console.log("Waiting for confirmations...");
+  // const receipt = await publicClient.waitForTransactionReceipt({ hash });
+  // console.log("Ballot contract deployed to:", receipt.contractAddress);
+
+  const parameters = process.argv.slice(2);
+  if (!parameters || parameters.length < 2)
+    throw new Error("Parameters not provided");
+  const contractAddress = parameters[0] as `0x${string}`;
+  if (!contractAddress) throw new Error("Contract address not provided");
+  if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+    throw new Error("Invalid contract address");
+  const proposalIndex = parameters[1];
+  if (isNaN(Number(proposalIndex))) throw new Error("Invalid proposal index");
+
+  
+  console.log("Proposal selected: ");
+  const proposal = (await publicClient.readContract({
+    address: contractAddress,
+    abi,
+    functionName: "proposals",
+    args: [BigInt(proposalIndex)],
+  })) as any[];
+  const name = hexToString(proposal[0], { size: 32 });
+  console.log("Voting to proposal", name);
+  console.log("Confirm? (Y/n)");
+
+  
+  const voter = createWalletClient({
+      account,
+      chain: sepolia,
+      transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+
+  const hash = await voter.writeContract({
+    address: contractAddress,
+    abi,
+    functionName: "vote",
+    args: [BigInt(proposalIndex)],
+});
+console.log("Transaction hash:", hash);
+console.log("Waiting for confirmations...");
+const receipt = await publicClient.waitForTransactionReceipt({ hash });
+console.log("Transaction confirmed");
+
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
